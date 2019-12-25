c17258716f0c78537d5abb784e28f66c57b44bc5
Emre Ergün <emreergnn@hotmail.com>
2019-12-25 17:50:55 +0200

    yorum satırı eklendi

--- TarziniYaratProject.DAL/Concrete/EntityFramework/Mapping/LikeMapping.cs
+++ TarziniYaratProject.DAL/Concrete/EntityFramework/Mapping/LikeMapping.cs
@@ -11,7 +11,7 @@ namespace TarziniYaratProject.DAL.Concrete.EntityFramework.Mapping
     public class LikeMapping : EntityTypeConfiguration<Like>
     {
         public LikeMapping()
-        {
+        {//asfhsfhdf
             HasKey(a => a.LikeID);
             Property(a => a.CommentID)
                 .IsOptional();
