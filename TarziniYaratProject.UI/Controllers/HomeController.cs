9d31b817c2ef7f7d57d4f08e572b2616935b8bdd
esraakann <46568960+esraakann@users.noreply.github.com>
2019-12-31 14:38:27 +0300

    Product sayfasına giriş yapıldı

--- TarziniYaratProject.UI/Controllers/HomeController.cs
+++ TarziniYaratProject.UI/Controllers/HomeController.cs
@@ -3,13 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TarziniYaratProject.BLL.Abstract.EntityServices;
 
 namespace TarziniYaratProject.UI.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
+        IProductService _productService;
+        public HomeController(IProductService productService)
+        {
+            _productService = productService;
+        }
         public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Products()
+        {
+            return View(_productService.GetAll().ToList());
+        }
+        public ActionResult Contact()
         {
             return View();
         }
