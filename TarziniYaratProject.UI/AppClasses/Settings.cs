cd915d8632c4aec71e26100395a4b388536f943e
Emre Ergün <emreergnn@hotmail.com>
2019-12-31 12:58:32 +0200

    Admin Paneli Son İşlemler yapıldı

---
+++ TarziniYaratProject.UI/AppClasses/Settings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Drawing;
+using System.Configuration;
+
+namespace TarziniYaratProject.UI.AppClasses
+{
+    public class Settings
+    {
+        public static Size SliderImageSize
+        {
+            get
+            {
+                Size size = new Size();
+                size.Width = Convert.ToInt32(ConfigurationManager.AppSettings["SliderWidth"]);
+                size.Height = Convert.ToInt32(ConfigurationManager.AppSettings["SliderHeight"]);
+                return size;
+            }
+        }
+    }
+}
---
+++ TarziniYaratProject.UI/Areas/Admin/Controllers/AdminSiteProcessesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TarziniYaratProject.BLL.Abstract.EntityServices;
+using TarziniYaratProject.Entities.Models;
+using TarziniYaratProject.UI.AppClasses;
+using Image = System.Drawing.Image;
+
+namespace TarziniYaratProject.UI.Areas.Admin.Controllers
+{
+    public class AdminSiteProcessesController : Controller
+    {
+        ISliderImageService _sliderImageService;
+        public AdminSiteProcessesController(ISliderImageService sliderImageService)
+        {
+            _sliderImageService = sliderImageService;
+        }
+
+        public ActionResult SliderList()
+        {
+            List<SliderImage> sliders = _sliderImageService.GetAll() as List<SliderImage>;
+            return View(sliders);
+        }
+
+        public ActionResult CreateSliderImage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateSliderImage(HttpPostedFileBase fileUpload)
+        {
+            if (fileUpload!=null)
+            {
+                Image image = Image.FromStream(fileUpload.InputStream);
+                Bitmap bitmap = new Bitmap(image, Settings.SliderImageSize);
+                string extension = "/Images/Sliders/" + Guid.NewGuid() + Path.GetExtension(fileUpload.FileName);
+                bitmap.Save(Server.MapPath(extension));
+
+                SliderImage slider = new SliderImage();
+                slider.ImagePath = extension;
+                slider.IsActive = true;
+                _sliderImageService.Insert(slider);
+            }
+            return RedirectToAction("SliderList","Admin/AdminSiteProcesses"); 
+        }
+    }
+}
