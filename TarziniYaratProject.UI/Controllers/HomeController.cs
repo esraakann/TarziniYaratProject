b7551d7d9b51de91d3c1f333dc5f62626b4de3bf
Fatih Karademir <fatihkrdmr24@gmail.com>
2020-01-01 15:55:24 +0300

    son durum

--- TarziniYaratProject.UI/Controllers/HomeController.cs
+++ TarziniYaratProject.UI/Controllers/HomeController.cs
@@ -13,5 +13,13 @@ namespace TarziniYaratProject.UI.Controllers
         {
             return View();
         }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
     }
 }
